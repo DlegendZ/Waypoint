2c8b3e4796ca31b3db9c402d52a37272989ea4c8
DlegendZ <raynaldarvanlim@gmail.com>
2026-07-14 21:12:52 +0800

    fix: authorized http request coming from test.html

--- App/src/main/java/com/raynald/waypoint/config/SecurityConfig.java
+++ App/src/main/java/com/raynald/waypoint/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/drivers/**").hasRole("DRIVER")
+//                        .requestMatchers("/test.html").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
