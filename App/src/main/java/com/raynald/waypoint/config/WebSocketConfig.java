53fe745deeaef4fd60f5b0db1d41895f6bb44afa
DlegendZ <raynaldarvanlim@gmail.com>
2026-07-14 18:03:35 +0800

    feat: wire authentication into the WebSocket handshake

--- App/src/main/java/com/raynald/waypoint/config/WebSocketConfig.java
+++ App/src/main/java/com/raynald/waypoint/config/WebSocketConfig.java
@@ -1,14 +1,22 @@
 package com.raynald.waypoint.config;
 
+import com.raynald.waypoint.security.JwtCookieChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtCookieChannelInterceptor jwtCookieChannelInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
@@ -20,5 +28,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtCookieChannelInterceptor);
     }
 }
